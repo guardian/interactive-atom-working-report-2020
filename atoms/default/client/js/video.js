7670cf523809fa4a0f52bb5cd0ab945bbb8c3a08
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-12-14 17:58:56 +0100

    fix scope

--- atoms/default/client/js/video.js
+++ atoms/default/client/js/video.js
@@ -6,14 +6,14 @@ function setupVideo() {
 
   if (videoFigure) {
     const videoAtom = document.querySelector('.youtube-media-atom');
+    if (videoFigure && videoAtom) {
+      let videoOverlayEl = document.createElement('div');
+      videoOverlayEl.classList.add('wr-video__overlay');
+      videoOverlayEl.innerHTML = `<div class='wr-video__label'><strong>The Guardian's best journalism across the year</strong>Lorem insight dolor sit into the workings of the and Australia, over the last year</div><div class='wr-video__play'><svg width="29" height="25" viewBox="0 0 29 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M28.709 13.166v-1.237L1.149.534.133 1.33v22.437l1.016.706 27.56-11.306z" fill="#FFE500"/></svg></div>`;
+
+      videoAtom.appendChild(videoOverlayEl);
+    }
   }
 
-  if (videoFigure && videoAtom) {
-    let videoOverlayEl = document.createElement('div');
-    videoOverlayEl.classList.add('wr-video__overlay');
-    videoOverlayEl.innerHTML = `<div class='wr-video__label'><strong>The Guardian's best journalism across the year</strong>Lorem insight dolor sit into the workings of the and Australia, over the last year</div><div class='wr-video__play'><svg width="29" height="25" viewBox="0 0 29 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M28.709 13.166v-1.237L1.149.534.133 1.33v22.437l1.016.706 27.56-11.306z" fill="#FFE500"/></svg></div>`;
-
-    videoAtom.appendChild(videoOverlayEl);
-  }
 
 }
